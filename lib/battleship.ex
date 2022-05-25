e7365fe81d1b7e06231203c40ab9cfef15c6d34c
Preston Jarnagin <preston@homebot.ai>
2022-05-24 19:28:27 -0600

    Initial commit

---
+++ lib/battleship.ex
@@ -0,0 +1,18 @@
+defmodule Battleship do
+  @moduledoc """
+  Documentation for `Battleship`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Battleship.hello()
+      :world
+
+  """
+  def start do
+
+  end
+end
