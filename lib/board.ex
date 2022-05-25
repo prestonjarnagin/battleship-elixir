a348558f28a2e048aa4874684f62ae600aae1efd
Preston Jarnagin <preston@homebot.ai>
2022-05-24 20:11:43 -0600

    Add typespecs to `Board`

--- lib/board.ex
+++ lib/board.ex
@@ -1,23 +1,28 @@
 defmodule Board do
+
+  @spec init(pos_integer(), pos_integer()) :: list()
   def init(w, h) do
     Enum.map(1..h, fn i -> build_row([], w) end)
   end
 
+  @spec build_row(list(), pos_integer()) :: list()
   def build_row(board_in_progress, w) do
     Enum.map(1..w, fn x -> nil end)
   end
 
+  @spec width(list()) :: pos_integer()
   def width(board) do
     List.first(board)
     |> length()
   end
 
+  @spec width(list()) :: pos_integer()
   def height(board) do
     length(board)
   end
 
   def place_ship(ship_length, cell, oriantation, board) do
-    coordinate = decode_coordinates(cell)
+    coordinate = decode_coordinate(cell)
 
     list = get_cells_ship_will_cover(ship_length, coordinate, oriantation)
     all_coordinates_within_bounds?(list, board)
@@ -25,24 +30,21 @@ defmodule Board do
     # TODO: More here
   end
 
+  @spec all_coordinates_within_bounds?(list(), list()) :: boolean()
   def all_coordinates_within_bounds?(coordinates, board) do
-  # > [%{x: 1, y: 3}, %{x: 2, y: 3}]
-
     Enum.all?(coordinates, fn coord ->
       coordinate_within_bounds?(coord, board)
     end)
-    # true
   end
 
+  @spec coordinate_within_bounds?(%{x: pos_integer(), y: pos_integer()}, list()) :: boolean()
   def coordinate_within_bounds?(coordinate, board) do
-    # %{x:, y:}
     coordinate.x <= width(board)
     coordinate.y <= height(board)
-    # true
   end
 
+  @spec get_cells_ship_will_cover(pos_integer(), %{x: pos_integer(), y: pos_integer()}, String.t()) :: list()
   def get_cells_ship_will_cover(length, coordinate, oriantation) do
-    # > (4, %{x: 1, y: 3}, "h")
 
     Enum.map(0..length - 1, fn index ->
       case oriantation do
@@ -50,19 +52,15 @@ defmodule Board do
         "v" -> %{x: coordinate.x, y: coordinate.y  + index}
       end
     end)
-    # > [
-      #   %{x: 1, y: 3}, %{x: 2, y: 3}, %{x: 3, y: 3}, %{x: 4, y: 3}
-    # ]
   end
 
-  def decode_coordinates(coordinate) do
-    # > "B1"
+  @spec decode_coordinate(String.t()) :: %{x: pos_integer(), y: pos_integer()}
+  def decode_coordinate(coordinate) do
     [unconverted_x_coord | [y_coord]] = String.split(coordinate, "", trim: true)
 
     <<partly_converted_x_coord::utf8>> = unconverted_x_coord
     x_coord = partly_converted_x_coord - 64
 
     %{x: x_coord, y: String.to_integer(y_coord)}
-    # > %{x: 2, y: 1}
   end
 end
