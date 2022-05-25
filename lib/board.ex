541362129009f4e1cdf2de88820bd0ae9dd889bf
Preston Jarnagin <preston@homebot.ai>
2022-05-24 20:22:26 -0600

    Use new coordinate() type

--- lib/board.ex
+++ lib/board.ex
@@ -39,13 +39,11 @@ defmodule Board do
     end)
   end
 
-  @spec coordinate_within_bounds?(%{x: pos_integer(), y: pos_integer()}, list()) :: boolean()
   @spec coordinate_within_bounds?(coordinate(), list()) :: boolean()
   def coordinate_within_bounds?(coordinate, board) do
     coordinate.x <= width(board) and coordinate.y <= height(board)
   end
 
-  @spec get_cells_ship_will_cover(pos_integer(), %{x: pos_integer(), y: pos_integer()}, String.t()) :: list()
   @spec get_cells_ship_will_cover(pos_integer(), coordinate(), String.t()) :: list()
   def get_cells_ship_will_cover(length, coordinate, oriantation) do
 
